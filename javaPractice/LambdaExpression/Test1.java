bdc39971df2704c6bbe20874bf2680d5812cc678
javaMole <abhinareshit@gmail.com>
2024-09-14 17:15:39 +0530

    Basic lambda expression program

---
+++ javaPractice/LambdaExpression/Test1.java
@@ -0,0 +1,16 @@
+class Test1
+{
+	public static void main(String[] args)
+	{
+	I1 tv1=	()->{System.out.println("This is m1 method");};
+		tv1.m1();
+	}
+}
+
+@FunctionalInterface
+interface I1
+{
+	
+	void m1();
+
+}
