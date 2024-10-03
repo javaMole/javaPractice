080b441ce67106cca3f26c0cad5c2587bf891b7b
javaMole <abhinareshit@gmail.com>
2024-10-03 12:09:55 +0530

    Generics concept started

---
+++ javaPractice/Generics/Test1.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+class Test
+{
+	public static void main(String[] args)
+	{
+		ArrayList o1=new ArrayList();
+		o1.add("raj");
+		o1.add(55);
+
+		String s1=(String)o1.get(0);
+		System.out.println(s1);
+		
+		/*
+		problem 1
+		In arraylist I can add all object 
+		we can store different objects in arraylist 
+
+		but in some case I want to strictly store a particular object  then it is not possible 
+
+
+		*/
+
+		/*
+			Problem 2
+			example during retrieving  an Object in collection 
+			It will give type casting problems 
+				
+				String s1=o1.get(0); // it will lead to CE
+
+				Here the get method will give you an Object type object it is mandatory for us to downcast that object
+			
+				String s1=(String)o1.get(0);
+
+		*/
+
+		/*
+			Solution:
+				due to the above following problems generics were introduced to solve 
+					Type safety
+
+					Type casting Problems
+
+
+		*/
+
+
+
+	}
+}
